4e138d23763d89ed0312fdf99a7152b581ca6c33
vedant1100 <dahalevedant7@gmail.com>
2025-12-20 18:16:38 -0800

    Database Connection

---
+++ DynamicAllocator/structDynamicAllocation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class memory{
+    string name;
+    string phoneNo;
+    string topic;
+    double fee;
+};
+
+void inputSpeaker(Speaker )
+
+int main(){
+    
+    return 0;
+}
